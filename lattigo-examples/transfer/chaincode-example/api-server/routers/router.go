9c7c6fd4e1364241be21fbe1adb2da5f2ecd01d6
Shitaibin <hz_stb@163.com>
2020-08-05 11:48:57 +0800

    feat: 增加跨域

--- lattigo-examples/transfer/chaincode-example/api-server/routers/router.go
+++ lattigo-examples/transfer/chaincode-example/api-server/routers/router.go
@@ -10,10 +10,36 @@ package routers
 import (
 	"api-server/controllers"
 
+	"github.com/astaxie/beego/plugins/cors"
+
 	"github.com/astaxie/beego"
 )
 
 func init() {
+	// beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+	// 	// AllowAllOrigins:  true,
+	// 	AllowMethods:     []string{"POST", "GET"},
+	// 	AllowHeaders:     []string{"Origin", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type", "x-requested-with", "no-referrer-when-downgrade"},
+	// 	ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type", "Access-Control-Allow-Origin"},
+	// 	AllowCredentials: true,
+	// 	AllowOrigins:     []string{"*"},
+	// }))
+
+	//InsertFilter是提供一个过滤函数
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		// 允许访问所有源
+		AllowAllOrigins: true,
+		// 可选参数"GET", "POST", "PUT", "DELETE", "OPTIONS" (*为所有)
+		// 其中Options跨域复杂请求预检
+		AllowMethods: []string{"*"},
+		// 指的是允许的Header的种类
+		AllowHeaders: []string{"*"},
+		// 公开的HTTP标头列表
+		ExposeHeaders: []string{"Content-Length"},
+		// 如果设置，则允许共享身份验证凭据，例如cookie
+		AllowCredentials: true,
+	}))
+
 	ns := beego.NewNamespace("/v1",
 		// beego.NSNamespace("/object",
 		// 	beego.NSInclude(
--- lattigo-examples/transfer/chaincode-example/api-server/run.sh
+++ lattigo-examples/transfer/chaincode-example/api-server/run.sh
@@ -2,5 +2,5 @@
 
 go get github.com/go-kit/kit@v0.8.0
 
-bee run -downdoc=true -gendoc=true
-
+#bee run -downdoc=true -gendoc=true
+bee run
